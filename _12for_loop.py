5324ab7b61bf20c6bd0dbb7019f95d82ff7cd9a5
chetanchaure07 <chetanchaure05@gmail.com>
2025-11-04 21:20:16 +0530

    Add files via upload

---
+++ _12for_loop.py
@@ -0,0 +1,9 @@
+#A for loop is used to repeat a block of code multiple times.
+#it is mostly used when you want to iterate through a sequence
+#(like list, string, tuple or range)
+for i in range(5):
+    print("Hello")
+for i in range(1,6):
+    print(i)
+for i in "python":
+    print(i)
---
+++ _3comment.py
@@ -0,0 +1,12 @@
+# single line comment
+
+"""This is 
+Multiline 
+comment"""
+
+'''This is 
+Mulitiline 
+comment'''
+
+# we often use this type of single line comment and the shortcut of single line comment is 
+# (ctrl + /)

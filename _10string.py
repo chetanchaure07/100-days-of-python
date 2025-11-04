f2298f90f73633eba0de9ee2726b40609e49397c
chetanchaure07 <chetanchaure05@gmail.com>
2025-11-04 21:25:37 +0530

    Delete _10string.py

--- _10string.py
+++
@@ -1,9 +0,0 @@
-name = "chetan"
-#index = "012345" positive index
-#index = "-6-5-4-3-2-1"  negative index
-# string slicing
-print(name[:])
-print(name[3:6]) #[start : last-1 : step]
-print(len(name))
-print(name[-3:])
-print(name[::2])

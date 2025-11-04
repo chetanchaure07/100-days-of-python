e1a618fbbb72b247ce2759a9b412e9933f2594ac
chetanchaure07 <chetanchaure05@gmail.com>
2025-11-04 21:26:12 +0530

    Delete _5variable.py

--- _5variable.py
+++
@@ -1,9 +0,0 @@
-# A variable is a name that stores a value in memory you can think of it like a box that holds same data
-
-x = 10              #integer value
-y = "python"        #string value
-z = 2.5             #float value
-
-print(type(x))
-print(type(y))
-print(type(z))

2626c74a5745d8d4fdb439a2b0038b543083e937
chetanchaure07 <chetanchaure05@gmail.com>
2025-11-07 18:22:27 +0530

    Add files via upload

---
+++ _4escape.py
@@ -0,0 +1,11 @@
+a = "Lorem ipsum dolor\n sit amet consectetur adipisicing elit.\t\t Veritatis enim error \\necessitatibus \"alias\" \'nulla\' quibusdam\b animi perspiciatis voluptatem delectus reiciendis suscipit beatae omnis dolore optio eveniet adipisci corrupti consectetur nemo corporis itaque saepe Eligendi voluptate."
+
+print(a)
+print(r"C:\Users\cheta\.vscode\100 days of python")   #For print any path use r before string 
+
+#\n new line
+#\t tab line
+#\\ backslash
+#\" double quote
+#\' single quote
+#\b backslash space
